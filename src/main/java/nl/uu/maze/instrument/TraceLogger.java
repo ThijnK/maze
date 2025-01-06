c106fa81830155199dd78b4994b88183a28c37ea
Thijn <thijnkroon@gmail.com>
2025-01-06 11:52:59 +0100

    chore: write trace files to logs folder

--- src/main/java/nl/uu/maze/instrument/TraceLogger.java
+++ src/main/java/nl/uu/maze/instrument/TraceLogger.java
@@ -5,11 +5,11 @@ import java.io.IOException;
 import java.io.PrintWriter;
 
 public class TraceLogger {
-    private static final String FILE_NAME = "trace.log";
+    private static final String FILE_PATH = "logs/trace.log";
 
     // TODO: could probably be made more efficient
     public static void log(String message) {
-        try (FileWriter fw = new FileWriter(FILE_NAME, true);
+        try (FileWriter fw = new FileWriter(FILE_PATH, true);
                 PrintWriter pw = new PrintWriter(fw)) {
             pw.println(message);
         } catch (IOException e) {
