a1a1ce2d9e579bce04a6c1f8cddad2c9f5cc9818
Thijn <thijnkroon@gmail.com>
2025-04-04 20:45:03 +0200

    chore: update example class

--- src/main/java/nl/uu/maze/example/ExampleClass.java
+++ src/main/java/nl/uu/maze/example/ExampleClass.java
@@ -4,48 +4,16 @@ package nl.uu.maze.example;
  * Example class to test the application on.
  */
 public class ExampleClass {
-    Inner inner;
 
-    public ExampleClass(int x) {
-        inner = new Inner();
-        inner.x = x;
-    }
-
-    public int aliasing(int[] arr, int[] arr2) {
-        if (arr2 == null || arr2.length == 0 || arr2[0] != 77 || arr.length == 0) {
-            return -1;
-        }
-
-        if (arr == arr2) {
+    public static int exampleMethod(int[] arr) {
+        if (arr[0] == 77) {
             return 1;
+        } else if (arr[1] == 88) {
+            return 2;
+        } else if (arr[2] == 99) {
+            return 3;
         } else {
-            return 0;
+            return 4;
         }
-    }
-
-    public int multiarray(int[][] arr) {
-        if (arr.length == 0 || arr[0].length == 0) {
-            return -1;
-        }
-
-        if (arr[0][0] == 77) {
-            return 1;
-        } else {
-            return 0;
-        }
-    }
-
-    public int instancefield() {
-        if (inner.x > 0) {
-            return 1;
-        } else if (inner.x < 0) {
-            return -1;
-        } else {
-            return 0;
-        }
-    }
-
-    public static class Inner {
-        int x;
     }
 }
