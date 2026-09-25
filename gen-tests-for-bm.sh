0f38fa1e74f52567f2f787f02ec5406dfdeb0a0e
ThijnK <71406035+ThijnK@users.noreply.github.com>
2026-09-24 20:48:08 -0400

    fix(benchmarks): use current MAZE command-line options

--- gen-tests-for-bm.sh
+++ gen-tests-for-bm.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
+set -eu
 # this is a script for generating tests for the CUTs
 # in nl.uu.maze.benchmarks
 budget=30
 strategy=BFS
 depth=400
 #depth=200
-MAZE=./target/maze-1.1-jar-with-dependencies.jar
+MAZE=./target/maze-1.1.1-jar-with-dependencies.jar
 BM=(AckermannPeter
     BinarySearch
     BinaryTree
@@ -30,7 +31,6 @@ BM=(AckermannPeter
 
 for CUT in "${BM[@]}"; do
    echo "=== Generating tests for ${CUT}"
-   #   mvn exec:java -Dexec.args="--classpath=./target/classes --classname=nl.uu.maze.benchmarks.${CUT} --minimalistic-suite=true -s=${strategy} -b=${budget} --constrain-FP-params-to-normal-numbers=true --check-divbyZero=true --max-array-size=10 --max-depth=400 --path-length-cov=1 --output-path=./tmp/"
-   java -Xmx2500m -jar ${MAZE} --classpath=./target/classes --classname=nl.uu.maze.benchmarks.${CUT} --minimalistic-suite=true -s=${strategy} -b=${budget} --constrain-FP-params-to-normal-numbers=true --check-divbyZero=true --max-array-size=10 --max-depth=${depth} --path-length-cov=1 --output-path=./tmp/
+   java -Xmx2500m -jar "${MAZE}" --classpath=./target/classes --class-name=nl.uu.maze.benchmarks.${CUT} --minimization=true -s=${strategy} -b=${budget} --constrain-fp-params-to-normal-numbers=true --check-division-by-zero=true --max-array-size=10 --max-depth=${depth} --path-length-coverage=1 --output-path=./tmp/
    echo 'package nl.uu.tests.maze.generated.benchmarks;' | cat - ./tmp/${CUT}Test.java > ./tmp/haha && mv ./tmp/haha ./tmp/${CUT}Test.java
 done
